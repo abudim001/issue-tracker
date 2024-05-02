2b7fbfc28443745ecfd4d82d3dc0c611aaaec84c
Adem Hussen <abudim001@gmail.com>
2024-05-02 12:40:11 -0700

    Style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,13 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import { PiBugFill } from "react-icons/pi";
+import { PiBugBeetleFill } from "react-icons/pi";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
 const NavBar = () => {
+  const pathname = usePathname();
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -10,13 +15,17 @@ const NavBar = () => {
   return (
     <nav className="flex space-x-6 border-b bm-5 px-5 h-14 items-center">
       <Link href="/">
-        <PiBugFill />
+        <PiBugBeetleFill />
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
           <Link
             key={link.href}
-            className="text-gray-400 hover:text-gray-900 transition-colors"
+            className={classnames({
+              "text-blue-900 underline": link.href === pathname,
+              "text-gray-500": link.href !== pathname,
+              "hover:text-gray-800 transition-colors": true,
+            })}
             href={link.href}
           >
             {" "}
